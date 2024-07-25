1e2543a01187118ff67451654ea295f8ec9af843
daeun <daeun900@naver.com>
2024-07-25 16:21:02 +0900

    240725_공지사항 연결

--- src/screens/Notice.js
+++ src/screens/Notice.js
@@ -1,22 +1,23 @@
-import React, { useEffect , useContext} from "react";
+import React, { useEffect , useContext, useState} from "react";
 import { StyleSheet, Platform,Text, View, Image, useWindowDimensions, ScrollView,FlatList} from "react-native";
 import styled from "styled-components/native";
 import { TopSec} from "../components";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { UserContext } from "../context/userContext";
 import { Entypo } from '@expo/vector-icons';
+import axios from 'axios';
 
 const Container = styled.View`
   flex: 1;
   background-color:#fff;
 `
 const FlexBox = styled.View`
-  flex-direction:row;
+  flex-direction: row;
 `
 const MidTxt = styled.Text`
   font-size: 16px;
   line-height: 24px;
-  margin-bottom:10px;
+  margin-bottom: 10px;
 `
 const SmallTxt = styled.Text`
   font-size: 14px;
@@ -25,69 +26,70 @@ const SmallTxt = styled.Text`
 `
 const NoticeWrap = styled.TouchableOpacity`
   padding: 20px;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  border-bottom-width:1px;
-  border-color:#DEDEDE;
+  padding-right: 40px;
+  border-bottom-width: 1px;
+  border-color: #DEDEDE;
+  position: relative;
 `
-
-const DATA = [
-  {
-    id: '1',
-    title: '공지사항 제목 1',
-    date: '2024-07-25',
-    views: 100,
-  },
-  {
-    id: '2',
-    title: '공지사항 제목 2',
-    date: '2024-07-24',
-    views: 150,
-  },
-  {
-    id: '3',
-    title: '공지사항 제목 3',
-    date: '2024-07-23',
-    views: 200,
-  },
-];
-
-const Item = ({ title, date, views , navigation}) => (
-  <NoticeWrap onPress={() => navigation.navigate("NoticeView")}>
-    <View>
-      <MidTxt>{title}</MidTxt>
-      <FlexBox>
-        <SmallTxt>{date}</SmallTxt>
-        <SmallTxt style={{marginHorizontal:5}}>ㅣ</SmallTxt>
-        <SmallTxt>조회수 {views}</SmallTxt>
-      </FlexBox>
-    </View>
-    <Entypo name="chevron-thin-right" size={20} color="#767676" />
+const Icon = styled.View`
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  margin-top: 10px;
+`
+const Item = ({ title, date, views,idx, navigation }) => (
+  <NoticeWrap onPress={() => navigation.navigate("NoticeView",{idx})}>
+    <MidTxt>{title}</MidTxt>
+    <FlexBox>
+      <SmallTxt>{date}</SmallTxt>
+      <SmallTxt style={{ marginHorizontal: 5 }}>ㅣ</SmallTxt>
+      <SmallTxt>조회수 {views}</SmallTxt>
+    </FlexBox>
+    <Icon>
+      <Entypo name="chevron-thin-right" size={20} color="#767676" />
+    </Icon>
   </NoticeWrap>
 );
 
-const Notice =  ({ navigation }) => {
-  const insets = useSafeAreaInsets(); //아이폰 노치 문제 해결
-  const { userNm, updateUserNm  } = useContext(UserContext);
+const Notice = ({ navigation }) => {
+  const insets = useSafeAreaInsets(); // 아이폰 노치 문제 해결
+  const { userNm, updateUserNm } = useContext(UserContext);
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     updateUserNm();
+
+    // 서버에서 데이터 가져오기
+    axios.post('http://new.hrdeedu.com/mobileTest/notice_list.php')
+      .then(response => {
+        const formattedData = response.data.notiInfo.map((item) => ({
+          title: item[0],
+          date: item[1],
+          views: item[2],
+          idx: item[3]
+        }));
+        setData(formattedData);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }, []);
 
   const renderItem = ({ item }) => (
-    <Item title={item.title} date={item.date} views={item.views} navigation={navigation} />
+    <Item title={item.title} date={item.date} views={item.views} idx={item.idx} navigation={navigation} />
   );
+
   return (
-    <View insets={insets} style={{flex:1}}>
-        <TopSec name={userNm}/>
-        <Container contentContainerStyle={{ paddingBottom: insets.bottom}}>
+    <View insets={insets} style={{ flex: 1 }}>
+      <TopSec name={userNm} />
+      <Container contentContainerStyle={{ paddingBottom: insets.bottom }}>
         <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => item.idx}
         />
-        </Container>
-      </View>
+      </Container>
+    </View>
   );
 };
 
